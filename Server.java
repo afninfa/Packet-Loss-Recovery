11e8301d3099768961f2d3de0a895fcdbc188553
Anthony Gambale <ants.gambale@gmail.com>
2025-11-05 17:30:01 +1100

    added while true loop to receiver thread

--- Server.java
+++ Server.java
@@ -32,7 +32,7 @@ public class Server {
             while (true) {
                 // TODO: Go through the ackedSeqNumbers and send anything which they have not
                 // yet ACKed. If all are ACKed, send the FIN packet and then exit.
-                
+
                 // TODO: Pause for 5 seconds (non-blocking because it's a virtual thread) and wait
                 // while the receiver thread marks off more ACKs in the set
             }
@@ -48,42 +48,44 @@ public class Server {
             Map<String, ClientData> clientIdToData = new HashMap<>();
             // Listen for packets
             byte[] buffer = new byte[BUFFER_SIZE];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            try {
-                socket.receive(packet);
-            } catch (Exception e) {
-                System.err.println("Failed while receiving data " + e.getMessage());
-                return;
-            }
-            Packet tcpPacket = Packet.packetFromString(
-                new String(packet.getData(), 0, packet.getLength())
-            );
-            String clientUniqueId = packet.getAddress() + ":" + packet.getPort();
-            switch (tcpPacket.type()) {
-                case PacketType.INIT -> {
-                    // Populate the clientIdToData map with this new client
-                    ClientData clientData = new ClientData(
-                        // This needs to be concurrent because it will be written to by this thread
-                        // upon receiving ACKs, and read from by the thread dedicated to the client
-                        ConcurrentHashMap.newKeySet(),
-                        packet.getAddress(),
-                        packet.getPort(),
-                        clientUniqueId
-                    );
-                    clientIdToData.put(clientUniqueId, clientData);
-                    System.out.println("[INFO] New client registered with ID " + clientUniqueId);
-                    // Spawn a virtual thread to handle sends to this client
-                    Thread.startVirtualThread(makeSenderRoutine(clientData));
-                }
-                case PacketType.ACK -> {
-                    // TODO: Add this sequence number to the client's set
-                    // DON'T need to do anything else, like send a FIN packet. The thread
-                    // dedicated to the client will handle this.
-                }
-                case PacketType.DATA, PacketType.FIN -> {
-                    System.err.println("[ERROR] Received " + tcpPacket.type().name() +
-                        " packet from client, aborting.");
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                try {
+                    socket.receive(packet);
+                } catch (Exception e) {
+                    System.err.println("Failed while receiving data " + e.getMessage());
                     return;
+                }
+                Packet tcpPacket = Packet.packetFromString(
+                    new String(packet.getData(), 0, packet.getLength())
+                );
+                String clientUniqueId = packet.getAddress() + ":" + packet.getPort();
+                switch (tcpPacket.type()) {
+                    case PacketType.INIT -> {
+                        // Populate the clientIdToData map with this new client
+                        ClientData clientData = new ClientData(
+                            // This needs to be concurrent because it will be written to by this thread
+                            // upon receiving ACKs, and read from by the thread dedicated to the client
+                            ConcurrentHashMap.newKeySet(),
+                            packet.getAddress(),
+                            packet.getPort(),
+                            clientUniqueId
+                        );
+                        clientIdToData.put(clientUniqueId, clientData);
+                        System.out.println("[INFO] New client registered with ID " + clientUniqueId);
+                        // Spawn a virtual thread to handle sends to this client
+                        Thread.startVirtualThread(makeSenderRoutine(clientData));
+                    }
+                    case PacketType.ACK -> {
+                        // TODO: Add this sequence number to the client's set
+                        // DON'T need to do anything else, like send a FIN packet. The thread
+                        // dedicated to the client will handle this.
+                    }
+                    case PacketType.DATA, PacketType.FIN -> {
+                        System.err.println("[ERROR] Received " + tcpPacket.type().name() +
+                            " packet from client, aborting.");
+                        return;
+                    }
                 }
             }
         };
